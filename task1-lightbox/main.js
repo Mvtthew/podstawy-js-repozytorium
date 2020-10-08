4ab2a99db8f31b27be4f9fa3629d6ec591bdfa6c
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-10-08 13:09:13 +0200

    :wrench: Lightbox

--- task1-lightbox/main.js
+++ task1-lightbox/main.js
@@ -1,1 +1,32 @@
-console.log('Hello world');
+const initClickableImages = () => {
+	const imagesElements = document.querySelectorAll('.gallery .image');
+	imagesElements.forEach((imageElement) => {
+		imageElement.addEventListener('click', handleGalleryImageClick);
+	});
+};
+
+const initCloseLightBoxButton = () => {
+	const closeButtonElement = document.getElementById('light-box-close');
+	closeButtonElement.addEventListener('click', toggleLightBox);
+};
+
+const initBgLightBox = () => {
+	const bgElement = document.getElementById('light-box-bg');
+	bgElement.addEventListener('click', toggleLightBox);
+};
+
+const handleGalleryImageClick = (event) => {
+	const bgImageUrl = event.target.style.backgroundImage;
+	const lightBoxImage = document.getElementById('light-box-image');
+	lightBoxImage.style.backgroundImage = bgImageUrl;
+	toggleLightBox();
+};
+
+const toggleLightBox = () => {
+	const lightBoxElement = document.getElementById('light-box');
+	lightBoxElement.classList.toggle('show');
+};
+
+initClickableImages();
+initCloseLightBoxButton();
+initBgLightBox();
